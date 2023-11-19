22e0dcaef494f7a93e3a2b539f359d410686a261
TaeHyun Kim <taehyun.kim1982@gmail.com>
2023-11-19 17:04:52 +0900

    Basic web app built. (Build OK)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+//! Run with
+//!
+//! ```not_rust
+//! cargo run -p kakaogpt-app-rs
+//! ```
+
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    println!("html loaded.");
+    Html("<h1>Hello, World!</h1>")
 }
