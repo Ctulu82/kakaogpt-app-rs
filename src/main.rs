3c40f0634826a63ee81c82fc4226dc279bbda547
TaeHyun Kim <taehyun.kim1982@gmail.com>
2023-11-20 19:36:29 +0900

    Updated rustfmt.toml & executed cargo fmt.

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,10 @@ async fn main() {
 	// run it
 	let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 	println!("listening on {}", addr);
-	axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+	axum::Server::bind(&addr)
+		.serve(app.into_make_service())
+		.await
+		.unwrap();
 }
 
 async fn handler() -> Html<&'static str> {
